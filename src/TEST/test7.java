febb5fdf33be1ef38bfa73bec8fa815868f591e5
lihui880518 <873257222@qq.com>
2017-03-21 15:00:00 +0800

    第二次提交

---
+++ src/TEST/test7.java
@@ -0,0 +1,19 @@
+package TEST;
+/**
+ * @Author fufu
+ * @Time 2017-3-21 下午02:59:28
+ * @Version 1.0
+ * <p>Description:</p>
+ */
+public class test7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("lihui");
+
+	}
+
+}
+ 
